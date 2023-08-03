5446c97273f4d85b53cee25a976fa78bd3e5207f
”chafaner <2771508305@qq.com>
2023-08-03 10:56:22 +0800

    全局异常处理

---
+++ common/service-util/src/main/java/com/atguigu/common/handler/GlobalExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.atguigu.common.handler;
+
+/**
+ * @Auther: 茶凡
+ * @ClassName GlobalExceptionHandler
+ * @date 2023/8/3 10:23
+ * @Description TODO
+ */
+public class GlobalExceptionHandler {
+}
